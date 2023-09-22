9c770dce7dc1a0027b619ae08a519823d9c4e0a6
andr3oix <72841646+andr3oix@users.noreply.github.com>
2023-09-22 14:45:32 +0300

    Update lemon.sh

--- lemon.sh
+++ lemon.sh
@@ -15,7 +15,7 @@ echo '
    ██       █████  ██ ████ ██ ██    ██ ██ ██  ██ 
    ██           ██ ██  ██  ██ ██    ██ ██  ██ ██ 
    ███████ ██████  ██      ██  ██████  ██   ████ 
-                                   github.com/h4ck3r0
+                                   github.com/Andr3oix
                            
 ' | lolcat                           
  echo -e "\e[1;31m [\e[32m√\e[31m] \e[1;91m by \e[1;36mMohammed \e[93m/ \e[100;92m Youtube.com/c/H4Ck3R0\e[0m"
